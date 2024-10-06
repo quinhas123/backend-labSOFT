f5af63d85f2b2f4fd6a242fadac69fc41122f2fb
Lucas Moreira Schirmbeck <lucas.schirmbeck@mbochip.com>
2024-10-06 17:44:49 -0300

    initialize spring project

---
+++ src/main/java/com/backend/labSOFT/LabSoftApplication.java
@@ -0,0 +1,13 @@
+package com.backend.labSOFT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabSoftApplication.class, args);
+	}
+
+}
